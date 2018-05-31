8b5db3b7ea8c1ea58e825bd5d369c884223a6b49
Gabriela Guedez Hamerlok <gaby.guedez@gmail.com>
2018-05-31 21:07:15 +0000

    created first pie chart, need to fix, rendering issues

--- assets/js/whiskeygraphs.js
+++ assets/js/whiskeygraphs.js
@@ -1,11 +1,34 @@
+// PENDING - invert dim (to country) to group (to whisky)
+// andy's examples: https://github.com/Mormoran/twitter_django/blob/master/static/js/graph.js
+
 queue()
     .defer(d3.csv, "assets/data/whiskey-insight.csv")
     .await(makeGraphs);
 
 
-function makeGraphs(error, salaryData) {
+function makeGraphs(error, whiskeyData) {
     
-    $.each(salaryData, function(key, value){
-        console.log(key, value);
+    var ndx = crossfilter(whiskeyData);
+    
+    // var whiskeyDim = ndx.dimension(dc.pluck("Whisky"));
+    
+    var whiskeyDim = ndx.dimension(function (d) {
+        return d["Whisky"];
     });
+    
+    // var whiskeyPerCountryGroup = whiskeyDim.group().reduceSum(dc.pluck("Country"));
+    
+    var whiskeyPerCountryGroup = whiskeyDim.group();
+    
+    var pieChart = dc.pieChart("#whiskey-per-country-chart");
+    
+    pieChart
+        .height(400)
+        // .slicesCap(4)
+        .radius(150)
+        .transitionDuration(1000)
+        .dimension(whiskeyDim)
+        .group(whiskeyPerCountryGroup);
+
+    dc.renderAll();
 }
