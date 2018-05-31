0cdc09587941d4a36258868b7f5f81bd689f4d48
Gabriela Guedez Hamerlok <gaby.guedez@gmail.com>
2018-05-31 20:32:45 +0000

    managed to get the csv to load, realised cloud 9 has an issue with the CDN I was using

--- assets/js/whiskeygraphs.js
+++ assets/js/whiskeygraphs.js
@@ -1,28 +1,11 @@
 queue()
-    .defer(d3.csv, "assets/data/whiskey-analysis.csv")
-    .await(makewhiskeygraphs);
+    .defer(d3.csv, "assets/data/whiskey-insight.csv")
+    .await(makeGraphs);
 
-function makewhiskeygraphs(whiskeyData){
+
+function makeGraphs(error, salaryData) {
     
-    console.log(typeof(whiskeyData));
-    
-    for (i = 0; i < whiskeyData.length; i++) { 
-        console.log(whiskeyData[i]);
-    }
-    
-    var ndx = crossfilter(whiskeyData);
-    
-    // parse data HERE
-    
-    var whiskey_dim = ndx.dimension(dc.pluck("Whisky"));
-    var whiskey_per_country = whiskey_dim.group().reduceSum(dc.pluck("country"));
-    
-    // dc.piechart("#whiskey-per-country-chart")
-    //     .height(300)
-    //     .radius(250)
-    //     .transitionDuration(1000)
-    //     .dimension(whiskey_dim)
-    //     .group(whiskey_per_country);
-        
-    // dc.renderAll();
+    $.each(salaryData, function(key, value){
+        console.log(key, value);
+    });
 }
