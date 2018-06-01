f7d5ba550079f105841d3b8d6b5d74bbb8d3bcc3
Gabriela Guedez Hamerlok <gaby.guedez@gmail.com>
2018-06-01 14:40:52 +0000

    got the map to render, trying to get the bubble graph

---
+++ assets/js/map.js
@@ -0,0 +1,36 @@
+queue()
+    .defer(d3.csv, "assets/data/whiskey-insight.csv")
+    .defer(d3.json, "https://raw.githubusercontent.com/johan/world.geo.json/master/countries.geo.json")
+    .await(makeGraphs);
+
+function makeGraphs(error, countriesJson, worldJson) {
+    
+    var ndx = crossfilter(countriesJson);
+    show_map_of_world(ndx, worldJson);
+    
+    dc.renderAll()
+}
+
+function show_map_of_world(ndx, worldJson) {
+    var countryDim = ndx.dimension(dc.pluck('country'));
+    var group = countryDim.group().reduceSum(dc.pluck('amount'));
+    var projection = d3.geo.mercator()
+        .center([0, 40])
+        .scale(100)
+        .rotate([-12, 0]);
+    dc.geoChoroplethChart("#whiskey-per-country-bubble-chart")
+        .width(1000)
+        .height(500)
+        .dimension(countryDim)
+        .group(group)
+        .colors(["#ff794d", "#8c8c8c"])
+        // .colorDomain([500, 50000])
+        .overlayGeoJson(worldJson["features"], "state", function(d) {
+            return d.id;
+        })
+        .projection(projection)
+        .title(function(p) {
+            console.log(p);
+            return "Country: " + p["key"];
+        });
+}
--- assets/js/whiskeygraphs.js
+++ assets/js/whiskeygraphs.js
@@ -2,25 +2,25 @@
 // andy's examples: https://github.com/Mormoran/twitter_django/blob/master/static/js/graph.js
 queue()
     .defer(d3.csv, "assets/data/whiskey-insight.csv")
+    .defer(d3.json, "https://raw.githubusercontent.com/johan/world.geo.json/master/countries.geo.json")
     .await(makeGraphs);
-
 
 function makeGraphs(error, whiskeyData) {
     
     var ndx = crossfilter(whiskeyData);
+    whiskey_per_country(ndx, whiskeyData);
     
-    // var countryDim = ndx.dimension(dc.pluck("Country"));
-    
+    dc.renderAll()
+}
+
+function whiskey_per_country(ndx) {
     var countryDim = ndx.dimension(function (d) {
         return d["Country"];
     });
-    
-    // var whiskeyPerCountryGroup = countryDim.group().reduceSum(dc.pluck("Whisky"));
-    
-    var whiskeyPerCountryGroup = countryDim.group();
-    
+     var whiskeyPerCountryGroup = countryDim.group();
+
     var pieChart = dc.pieChart("#whiskey-per-country-chart");
-    
+
     pieChart
         .height(400)
         // .slicesCap(4)
@@ -31,3 +31,31 @@ function makeGraphs(error, whiskeyData) {
 
     dc.renderAll();
 }
+
+// function makeGraphs(error, whiskeyData) {
+
+//     var ndx = crossfilter(whiskeyData);
+
+    // var countryDim = ndx.dimension(dc.pluck("Country"));
+
+    // var countryDim = ndx.dimension(function (d) {
+    //     return d["Country"];
+    // });
+
+    // var whiskeyPerCountryGroup = countryDim.group().reduceSum(dc.pluck("Whisky"));
+
+//     var whiskeyPerCountryGroup = countryDim.group();
+
+//     var pieChart = dc.pieChart("#whiskey-per-country-chart");
+
+//     pieChart
+//         .height(400)
+//         // .slicesCap(4)
+//         .radius(150)
+//         .transitionDuration(1000)
+//         .dimension(countryDim)
+//         .group(whiskeyPerCountryGroup);
+
+//     dc.renderAll();
+
+// }
