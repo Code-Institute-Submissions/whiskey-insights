d508d65d4c8db0443837df19fd0565a8e1a80a16
Gabriela Guedez Hamerlok <gaby.guedez@gmail.com>
2018-06-05 21:32:21 +0000

    changed the approach. Added a datamap and plan to place the bubbles on top of the map

--- assets/js/whiskeygraphs.js
+++ assets/js/whiskeygraphs.js
@@ -1,52 +1,138 @@
-// PENDING - invert dim (to country) to group (to whisky)
-// andy's examples: https://github.com/Mormoran/twitter_django/blob/master/static/js/graph.js
+$(document).ready(function() {
+    console.log( "ready!" );
+
 queue()
     .defer(d3.csv, "assets/data/whiskey-insight.csv")
-    .defer(d3.json, "https://raw.githubusercontent.com/johan/world.geo.json/master/countries.geo.json")
-    .await(makeGraphs);
+    .await(getCountryData);
 
-function makeGraphs(error, whiskeyData) {
+var element = document.getElementById("world-map");
+console.log(element);
+
+var countries = new Set();
+
+var whiskeyBubbles = new Object();
+
+function getCountryData(error, whiskeyData) {
+    for (i = 0; i < whiskeyData.length; i++) {
+        // console.log(whiskeyData[i]["Country"]);
+        if (whiskeyData[i]["Country"] in whiskeyBubbles) {
+            whiskeyBubbles[whiskeyData[i]["Country"]]++
+        } else {
+            whiskeyBubbles[whiskeyData[i]["Country"]] = 0;
+        }
+        countries.add(whiskeyData[i]["Country"]);
+    }
     
-    var ndx = crossfilter(whiskeyData);
-    whiskey_per_country(ndx, whiskeyData);
+    console.log(whiskeyBubbles);
     
-    dc.renderAll()
+    countries = Array.from(countries);
+    // console.log(countries);
+    
+    // for (i = 0; i < countries.length; i++) {
+    //     console.log(countries[i]);
+    // }
 }
 
-function whiskey_per_country(ndx) {
-    var countryDim = ndx.dimension(function (d) {
-        return d["Country"];
-    });
-     var whiskeyPerCountryGroup = countryDim.group();
+var bubble_map = new Datamap({
+  element: document.getElementById("world-map"),
+  geographyConfig: {
+    popupOnHover: false,
+    highlightOnHover: false
+  },
+  fills: {
+    defaultFill: '#ff794d',
+    BEL: 'yellow',
+    CNC: 'purple',
+    GBR: 'red',
+    FIN: 'black',
+    FRA: 'pink',
+    IND: 'brown',
+    IRL: 'green',
+    JPN: 'orange',
+    NLD: 'crimson',
+    ZAF: 'cyan',
+    SWE: 'darkgreen',
+    CHE: 'gold',
+    TWN: 'indigo',
+    AUT: 'lime',
+    USA: 'blue'
+  }
+});
 
-    var pieChart = dc.pieChart("#whiskey-per-country-chart");
+bubble_map.bubbles([
+  {
+    name: '',
+    radius: 10,
+    centered: 'BEL',
+    country: 'BEL',
+    yeild: 0,
+    fillKey: 'BEL',
+    // date: '1954-03-01'
+  },{
+    name: '',
+    radius: 10,
+    centered: 'CNC',
+    country: 'CNC',
+    yeild: 0,
+    fillKey: 'CNC',
+    // date: '1954-03-01'
+  },{
+    name: '',
+    radius: 10,
+    centered: 'GBR',
+    country: 'GBR',
+    yeild: 0,
+    fillKey: 'GBR',
+    // date: '1954-03-01'
+  },{
+    name: 'Not a bomb, but centered on Brazil',
+    radius: 23,
+    centered: 'BRA',
+    country: 'USA',
+    yeild: 0,
+    fillKey: 'USA',
+    date: '1954-03-01'
+  },{
+    name: 'Tsar Bomba',
+    radius: 70,
+    yeild: 50000,
+    country: 'USSR',
+    fillKey: 'RUS',
+    significance: 'Largest thermonuclear weapon ever tested—scaled down from its initial 100 Mt design by 50%',
+    date: '1961-10-31',
+    latitude: 73.482,
+    longitude: 54.5854
+  }
+], {
+  popupTemplate: function(geo, data) {
+    return '<div class="hoverinfo">Botles Reviewed:' + data.yield + '';
+// Exploded on ' + data.date + ' by the '  + data.country + ''
+  }
+});
 
-    pieChart
-        .height(400)
-        // .slicesCap(4)
-        .radius(150)
-        .transitionDuration(1000)
-        .dimension(countryDim)
-        .group(whiskeyPerCountryGroup);
 
-    dc.renderAll();
-}
 
 // function makeGraphs(error, whiskeyData) {
-
+    
 //     var ndx = crossfilter(whiskeyData);
 
-    // var countryDim = ndx.dimension(dc.pluck("Country"));
+    
+//     whiskey_per_country(ndx, whiskeyData);
+    
+//     dc.renderAll()
+// }
 
-    // var countryDim = ndx.dimension(function (d) {
-    //     return d["Country"];
-    // });
 
-    // var whiskeyPerCountryGroup = countryDim.group().reduceSum(dc.pluck("Whisky"));
 
-//     var whiskeyPerCountryGroup = countryDim.group();
+// // Pie chart below
 
-//     var pieChart = dc.pieChart("#whiskey-per-country-chart");
+// function whiskey_per_country(ndx) {
+//     var countryDim = ndx.dimension(function (d) {
+//         return d["Country"];
+//     });
+//      var whiskeyPerCountryGroup = countryDim.group();
+
+//     var pieChart = dc.pieChart("#whiskey-per-country-bubble-chart-overlay");
 
 //     pieChart
 //         .height(400)
@@ -57,5 +143,9 @@ function whiskey_per_country(ndx) {
 //         .group(whiskeyPerCountryGroup);
 
 //     dc.renderAll();
-
 // }
+
+
+
+// Document ready closing bracket
+});
