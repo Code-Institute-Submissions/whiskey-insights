a790aed154da43b8a49c69ee479fb8ddd2e38996
Gabriela Guedez Hamerlok <gaby.guedez@gmail.com>
2018-06-06 10:45:02 +0000

    cleaned code a bit, took note of what's pending. Not much more progress yet

--- assets/js/whiskeygraphs.js
+++ assets/js/whiskeygraphs.js
@@ -5,11 +5,6 @@ queue()
     .defer(d3.csv, "assets/data/whiskey-insight.csv")
     .await(getCountryData);
 
-// var element = document.getElementById("world-map");
-// console.log(element);
-
-// var countries = new Set();
-
 var whiskeyBubbles = new Object();
 
 function getCountryData(error, whiskeyData) {
@@ -22,18 +17,17 @@ function getCountryData(error, whiskeyData) {
         } else {
             whiskeyBubbles[whiskeyData[i]["Country"]] = 1;
         }
-        // countries.add(whiskeyData[i]["Country"]);
     }
     
     console.log(whiskeyBubbles);
-    
-    // countries = Array.from(countries);
-    // console.log(countries);
-    
-    // for (i = 0; i < countries.length; i++) {
-    //     console.log(countries[i]);
-    // }
 }
+
+// PENDING - Pass the radius into our chart (create five different sizes according to the value on whiskeyBubbles per country)
+// PENDING - Get the coordinates right
+// PENDING - Get the content for the hover tag to say how many bottles reviewd per country
+
+
+// Below, we start to create the individual bubbles
 
 var bubble_map = new Datamap({
   element: document.getElementById("world-map"),
@@ -44,7 +38,7 @@ var bubble_map = new Datamap({
   fills: {
     defaultFill: '#ff794d',
     BEL: 'yellow',
-    CNC: 'purple',
+    CAN: 'purple',
     GBR: 'red',
     FIN: 'black',
     FRA: 'pink',
@@ -63,47 +57,146 @@ var bubble_map = new Datamap({
 
 bubble_map.bubbles([
   {
-    name: '',
+    name: 'Belgium',
     radius: 10,
     centered: 'BEL',
     country: 'BEL',
     yeild: 0,
     fillKey: 'BEL',
-    // date: '1954-03-01'
+    latitude: 50.8503,
+    longitude: 4.3517
   },{
-    name: '',
-    radius: 10,
-    centered: 'CNC',
-    country: 'CNC',
+    name: 'Canada',
+    radius: 40,
+    centered: 'CAN',
+    country: 'CAN',
     yeild: 0,
-    fillKey: 'CNC',
-    // date: '1954-03-01'
+    fillKey: 'CAN',
+    latitude: 43.6532,
+    longitude: 79.3832
   },{
-    name: '',
+    name: 'England',
     radius: 10,
     centered: 'GBR',
     country: 'GBR',
     yeild: 0,
     fillKey: 'GBR',
-    // date: '1954-03-01'
+    latitude: 55.9533,
+    longitude: 3.1883
   },{
-    name: 'Not a bomb, but centered on Brazil',
-    radius: 23,
-    centered: 'BRA',
-    country: 'USA',
+    name: 'Finland',
+    radius: 10,
+    centered: 'FIN',
+    country: 'FIN',
     yeild: 0,
-    fillKey: 'USA',
-    date: '1954-03-01'
+    fillKey: 'FIN',
+    latitude: 61.9241,
+    longitude: 25.7482 
   },{
-    name: 'Tsar Bomba',
-    radius: 70,
+    name: 'France',
+    radius: 10,
     yeild: 50000,
-    country: 'USSR',
-    fillKey: 'RUS',
-    significance: 'Largest thermonuclear weapon ever tested—scaled down from its initial 100 Mt design by 50%',
-    date: '1961-10-31',
-    latitude: 73.482,
-    longitude: 54.5854
+    country: 'FRA',
+    fillKey: 'FRA',
+    latitude: 48.8566,
+    longitude: 2.3522
+  },{
+    name: 'India',
+    radius: 20,
+    yeild: 50000,
+    country: 'IND',
+    fillKey: 'IND',
+    latitude: 20.5937,
+    longitude: 78.9629
+  },{
+    name: 'Ireland',
+    radius: 35,
+    yeild: 50000,
+    country: 'IRL',
+    fillKey: 'IRL',
+    latitude: 53.1424,
+    longitude: 7.6921
+  },{
+    name: 'Japan',
+    radius: 35,
+    yeild: 50000,
+    country: 'JPN',
+    fillKey: 'JPN',
+    latitude: 36.2048,
+    longitude: 138.2529
+  },{
+    name: 'Netherlands',
+    radius: 10,
+    yeild: 50000,
+    country: 'NLD',
+    fillKey: 'NLD',
+    latitude: 52.1326,
+    longitude: 5.2913
+  },{
+    name: 'Scotland',
+    radius: 80,
+    centered: 'GBR',
+    country: 'GBR',
+    yeild: 0,
+    fillKey: 'GBR',
+    latitude: 55.9533,
+    longitude: 3.1883
+  },{
+    name: 'South Africa',
+    radius: 10,
+    yeild: 50000,
+    country: 'ZAF',
+    fillKey: 'ZAF',
+    latitude: 30.5595,
+    longitude: 22.9375
+  },{
+    name: 'Sweden',
+    radius: 20,
+    yeild: 50000,
+    country: 'SWE',
+    fillKey: 'SWE',
+    latitude: 60.1282,
+    longitude: 18.6435
+  },{
+    name: 'Switzerland',
+    radius: 10,
+    yeild: 50000,
+    country: 'CHE',
+    fillKey: 'CHE',
+    latitude: 46.818,
+    longitude: 8.2275
+  },{
+    name: 'Taiwan',
+    radius: 10,
+    yeild: 50000,
+    country: 'TWN',
+    fillKey: 'TWN',
+    latitude: 23.6978,
+    longitude: 20.9605
+  },{
+    name: 'Australia (Tasmania)',
+    radius: 10,
+    yeild: 50000,
+    country: 'AUT',
+    fillKey: 'AUT',
+    latitude: 41.4545,
+    longitude: 145.9707
+  },{
+    name: 'USA',
+    radius: 60,
+    yeild: 50000,
+    country: 'USA',
+    fillKey: 'USA',
+    latitude: 37.0902,
+    longitude: 95.7129
+  },{
+    name: 'Wales',
+    radius: 10,
+    yeild: 50000,
+    country: 'GBR',
+    fillKey: 'GBR',
+    latitude: 52.1307,
+    longitude: 3.7837
   }
 ], {
   popupTemplate: function(geo, data) {
